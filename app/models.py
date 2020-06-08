44eac3aa85e887efdca583ae639bbfb1f5c12122
Jianhua-Wang <jianhua.mert@gmail.com>
2020-06-08 12:02:22 +0800

    profile

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app
 from flask_login import UserMixin
 from . import db, login_manager
-import datetime
+from datetime import datetime
 
 class Permission:
     FOLLOW = 0x01
